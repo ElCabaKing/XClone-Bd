6ca6861c43fc404024ed64becb5f7d6a3ea43a77
ElCabaKing <davi.cabanilla@gmail.com>
2026-04-21 11:11:36 -0500

    refactor: rename user_role table to role and update related references; add role_user table for user-role associations

--- initDB/01.Tables/01.User.sql
+++ initDB/01.Tables/01.User.sql
@@ -4,17 +4,24 @@ CREATE TABLE user_status (
     description NVARCHAR(255) NULL
 );
 
-CREATE TABLE user_role (
+CREATE TABLE role (
     id TINYINT PRIMARY KEY,
     name NVARCHAR(50) NOT NULL UNIQUE,
     description NVARCHAR(255) NULL
+);
+CREATE TABLE role_user (
+    user_id UNIQUEIDENTIFIER NOT NULL,
+    role_id TINYINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT FK_Role_User_User FOREIGN KEY (user_id) REFERENCES [user](id),
+    CONSTRAINT FK_Role_User_Role FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
 INSERT INTO user_status (id, name, description) VALUES
 (1, 'active', 'User is active and can interact with the platform'),
 (2, 'inactive', 'User is inactive (cannot interact)');
 
-INSERT INTO user_role (id, name, description) VALUES
+INSERT INTO role (id, name, description) VALUES
 (1, 'admin', 'User has administrative privileges'),
 (2, 'common_user', 'User has standard privileges'),
 (3, 'verified_user', 'User has verified their identity');
@@ -26,11 +33,9 @@ CREATE TABLE [user] (
     email NVARCHAR(255) NOT NULL UNIQUE,
     password_hash NVARCHAR(255) NOT NULL,
     created_at DATETIME2 NOT NULL DEFAULT GETDATE(),
-    role_id TINYINT NULL DEFAULT 2,
     status_id TINYINT NULL DEFAULT 1,
     first_name NVARCHAR(255) NOT NULL,
     last_name NVARCHAR(255) NOT NULL,
     profile_picture_url NVARCHAR(255) NULL,
-    CONSTRAINT FK_User_Role FOREIGN KEY (role_id) REFERENCES user_role(id),
     CONSTRAINT FK_User_Status FOREIGN KEY (status_id) REFERENCES user_status(id)
 );
